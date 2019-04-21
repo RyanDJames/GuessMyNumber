a4cf8d2ca4b790ac7385e0ffe1924620a35034a7
Ryan James <james.ryan.d@outlook.com>
2019-04-21 16:13:28 -0400

    Made a game

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -10,6 +10,36 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+            int pick = 0;
+            Random random = new Random();
+            int number = random.Next(1, 100);
+            Console.WriteLine("Pick between 1 and 100");
+            int i = 0;
+            while(pick != number)
+            {
+                try
+                {
+                    pick = Convert.ToInt32(Console.ReadLine());
+
+                    if (pick > number)
+                    {
+                        Console.WriteLine("Your pick is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your pick is too low");
+
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Your choice has to be an interger");
+                }
+
+                i++;
+            }
+            Console.WriteLine("Thats right! You needed " + i + " Chances to guess the right number");
+            
         }
     }
 }
